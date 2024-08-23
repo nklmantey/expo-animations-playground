a9da36071106f62bab79ea99643fe02c2b72226a
Nana Kofi Larbi Mantey <nanakofilarbimantey@gmail.com>
2024-08-23 12:54:00 +0000

    removes unused files

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,8 +2,9 @@ import { Stack } from 'expo-router'
 
 export default function RootLayout() {
   return (
-    <Stack>
+    <Stack screenOptions={{ headerShown: false }}>
       <Stack.Screen name='index' />
+      <Stack.Screen name='showcase' />
     </Stack>
   )
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,60 @@
+import Card from '@/components/Card'
+import { Ionicons } from '@expo/vector-icons'
 import { View, Text } from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import styled from 'styled-components'
 
 export default function Index() {
   return (
-    <View>
-      <Text>Index</Text>
-    </View>
+    <SafeAreaContainer>
+      <HeaderContainer>
+        <HeaderTitle>Expo Playground</HeaderTitle>
+        <Ionicons
+          name='person-circle'
+          size={28}
+          color='#a1a1a1'
+        />
+      </HeaderContainer>
+      <HeaderDescription>
+        A collection of mobile interactions I have gathered from Twitter & decided to implement in my free time
+      </HeaderDescription>
+
+      <BodyContainer>
+        <Card
+          title='Animated FlatList'
+          description='Layout transition for input'
+          route='/showcase/animated-flatlist'
+        />
+      </BodyContainer>
+    </SafeAreaContainer>
   )
 }
+
+const SafeAreaContainer = styled(SafeAreaView)`
+  padding: 20px;
+`
+
+const HeaderContainer = styled(View)`
+  width: 100%;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+`
+
+const HeaderTitle = styled(Text)`
+  font-size:20px;
+  font-family: Georgia;
+`
+
+const HeaderDescription = styled(Text)`
+  font-size: 16px;
+  margin-top: 24px;
+  font-family: Georgia;
+  text-align: center;
+  line-height: 20px;
+`
+
+const BodyContainer = styled(View)`
+  width: 100%;
+  margin-top: 48px;
+`
